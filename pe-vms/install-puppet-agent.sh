ba0fb96dbb63cb2589ddf0098d2c86ff30e44303
Enis Inan <enis.inan@enis.inan-C02M48DGFD57>
2018-02-13 14:18:48 -0800

    Add more scripts

--- pe-vms/install-puppet-agent.sh
+++ pe-vms/install-puppet-agent.sh
@@ -3,14 +3,21 @@
 # Installs puppet agent on the given list of VMs
 # using the value of MASTER_HOST as the puppet master.
 
-master_vm=`printenv MASTER_HOST`
+master_vm="$1"
+shift
 
 if [[ -z "$master_vm" ]]; then
   echo "ERROR: no master found!"
   exit 1
 fi
 
+site_manifest="/etc/puppetlabs/code/environments/production/manifests/site.pp"
 for vm in "$@"; do
   ssh -t -oStrictHostKeyChecking=no root@$vm "curl -k https://${master_vm}:8140/packages/current/install.bash | sudo bash"
+  echo ""
+  echo "Going to sleep to wait for the agent cert to appear ..."
+  sleep 10
   ssh -oStrictHostKeyChecking=no root@$master_vm "puppet cert sign ${vm}" 
+  node_manifest=`echo "node \"'${vm}'\" {" && echo "  include cpp_dev_environment::facter_environment" && echo "}"`
+  ssh -oStrictHostKeyChecking=no root@$master_vm "(echo '' && echo '${node_manifest}') >> ${site_manifest}"
 done
--- pe-vms/setup-master-vm.sh
+++ pe-vms/setup-master-vm.sh
@@ -1,9 +1,25 @@
 #!/bin/bash
 
-vm=`./get-vms.sh`
+master_vm="$1"
+
+if [[ -z "$master_vm" ]]; then
+  echo "ERROR: Need to specify a master VM host!"
+  exit 1
+fi
 
 set -e
-./install-puppet-enterprise.sh "$vm"
-sed -i -e "s/export MASTER_HOST=.*/export MASTER_HOST=${vm}/" ~/.bash_profile 
-"./create-puppet-token.sh" "${vm}"
+#./install-puppet-enterprise.sh "$master_vm"
+#sed -i -e "s/export MASTER_HOST=.*/export MASTER_HOST=${master_vm}/" ~/.bash_profile 
+#"./create-puppet-token.sh" "${master_vm}"
 
+# Set-up some git stuff
+ssh -oStrictHostKeyChecking=no root@$master_vm "yum install -y git vim" 
+
+# Clone relevant modules. See if there's a way to use bolt with this
+MODULES="ekinanp/cpp_dev_environment"
+modules_root="/etc/puppetlabs/code/environments/production/modules/"
+for module in ${MODULES}; do
+  ssh -oStrictHostKeyChecking=no root@$master_vm "git clone https://github.com/${module}" 
+  dir=`echo "${module}" | gawk -F "/" {'print $2'}`
+  ssh -oStrictHostKeyChecking=no root@$master_vm "rm -rf "${modules_root}/${dir}" && mv ${dir} ${modules_root}" 
+done
