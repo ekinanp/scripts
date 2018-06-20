361157d01fd25a74c155a6fab523fb321d977c1c
Enis Inan <enis.inan@puppet.com>
2018-06-20 02:27:28 -0700

    (mait) Update w/ more stuff

--- pe-vms/setup-master-vm.sh
+++ pe-vms/setup-master-vm.sh
@@ -8,18 +8,18 @@ if [[ -z "$master_vm" ]]; then
 fi
 
 set -e
-#./install-puppet-enterprise.sh "$master_vm"
-#sed -i -e "s/export MASTER_HOST=.*/export MASTER_HOST=${master_vm}/" ~/.bash_profile 
-#"./create-puppet-token.sh" "${master_vm}"
+./install-puppet-enterprise.sh "$master_vm"
+sed -i -e "s/export MASTER_HOST=.*/export MASTER_HOST=${master_vm}/" ~/.bash_profile 
+"./create-puppet-token.sh" "${master_vm}"
 
-# Set-up some git stuff
-ssh -oStrictHostKeyChecking=no root@$master_vm "yum install -y git vim" 
-
-# Clone relevant modules. See if there's a way to use bolt with this
-MODULES="ekinanp/cpp_dev_environment"
-modules_root="/etc/puppetlabs/code/environments/production/modules/"
-for module in ${MODULES}; do
-  ssh -oStrictHostKeyChecking=no root@$master_vm "git clone https://github.com/${module}" 
-  dir=`echo "${module}" | gawk -F "/" {'print $2'}`
-  ssh -oStrictHostKeyChecking=no root@$master_vm "rm -rf "${modules_root}/${dir}" && mv ${dir} ${modules_root}" 
-done
+## Set-up some git stuff
+#ssh -oStrictHostKeyChecking=no root@$master_vm "yum install -y git vim" 
+#
+## Clone relevant modules. See if there's a way to use bolt with this
+#MODULES="ekinanp/cpp_dev_environment"
+#modules_root="/etc/puppetlabs/code/environments/production/modules/"
+#for module in ${MODULES}; do
+#  ssh -oStrictHostKeyChecking=no root@$master_vm "git clone https://github.com/${module}" 
+#  dir=`echo "${module}" | gawk -F "/" {'print $2'}`
+#  ssh -oStrictHostKeyChecking=no root@$master_vm "rm -rf "${modules_root}/${dir}" && mv ${dir} ${modules_root}" 
+#done
